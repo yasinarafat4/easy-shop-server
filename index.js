27080d1e8c9fe4d5f3890c995b22acf9dabfb5ea
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-10-23 18:22:33 +0600

    Successfully connected to MongoDB

--- index.js
+++ index.js
@@ -8,10 +8,41 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connect to MongoDB Database
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Hey Developer. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Define a route for the root URL
 app.get("/", (req, res) => {
   res.send("Easy Shop server is running");
 });
 
+// Start the Express server
 app.listen(port, () => {
   console.log(`Easy Shop is running on port ${port}`);
 });
