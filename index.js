be6cc7973f8dc32b3d90890cb46571402f575b3f
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-10-23 19:44:13 +0600

    Creating API to get All Products and Product Category done

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB Database
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx5eerd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,15 +24,43 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server
-    await client.connect();
+    // await client.connect();
+
+    // Collection
+    const productsCollection = client.db("EasyShopDB").collection("products");
+
+    // Getting all products data
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Getting products category
+    app.get("/products/:category", async (req, res) => {
+      console.log(req.params.category);
+      if (
+        req.params.category == "Mobiles" ||
+        req.params.category == "Clothes" ||
+        req.params.category == "Furniture" ||
+        req.params.category == "Books" ||
+        req.params.category == "Electronics"
+      ) {
+        const cursor = productsCollection.find({
+          category: req.params.category,
+        });
+        const result = await cursor.toArray();
+        return res.send(result);
+      }
+    });
+
+   
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Hey Developer. You successfully connected to MongoDB!"
-    );
+    console.log("Hey Developer. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
